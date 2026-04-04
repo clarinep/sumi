aa7ca7fe133ae0190c3819b26cefe590c22161c0
Clarinep <konovbaraputra@gmail.com>
2026-04-03 20:43:03 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,44 @@ mod config;
 mod renderer;
 mod routes;
 
-use std::{sync::Arc, time::Duration};
+use std::{
+    env,
+    error::Error,
+    net::SocketAddr,
+    sync::Arc,
+    thread,
+    time::Duration,
+};
 
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    routing::get,
+    Router,
+    serve,
+};
 use tower_http::timeout::TimeoutLayer;
 
 use crate::renderer::CardRenderer;
+use tokio::{
+    net::TcpListener,
+    runtime::Builder,
+    signal,
+};
 
 // we use microsoft mimalloc as it handles memory better
 // it will only help when tokio is running multi threads
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_var("RUST_LOG", "info");
+fn main() -> Result<(), Box<dyn Error>> {
+    env::set_var("RUST_LOG", "info");
     pretty_env_logger::init();
 
-    let cores = std::thread::available_parallelism().map(|n| n.get()).unwrap_or(4);
+    let cores = thread::available_parallelism().map(|n| n.get()).unwrap_or(4);
 
     log::info!("found {} cores - configuring..", cores);
 
-    tokio::runtime::Builder::new_multi_thread()
+    Builder::new_multi_thread()
         .enable_all()
         // limit tokio threads so server dont die.
         .max_blocking_threads(cores)
@@ -41,22 +58,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .route("/render/drop", get(routes::handle_render_drop))
                 .with_state(state)
                 .layer(TimeoutLayer::with_status_code(
-                    axum::http::StatusCode::REQUEST_TIMEOUT,
+                    StatusCode::REQUEST_TIMEOUT,
                     Duration::from_secs(30),
                 ));
 
-            let addr = std::net::SocketAddr::from(([127, 0, 0, 1], cfg.port));
-            let listener = tokio::net::TcpListener::bind(addr).await?;
+            let addr = SocketAddr::from(([127, 0, 0, 1], cfg.port));
+            let listener = TcpListener::bind(addr).await?;
 
             log::info!("server ready at http://{}", addr);
 
-            axum::serve(listener, app).with_graceful_shutdown(shutdown()).await?;
+            serve(listener, app).with_graceful_shutdown(shutdown()).await?;
 
             Ok(())
         })
 }
 
 async fn shutdown() {
-    tokio::signal::ctrl_c().await.expect("ctrl+c");
+    signal::ctrl_c().await.expect("ctrl+c");
     log::info!("you gave blair way too much caffeine..");
 }
