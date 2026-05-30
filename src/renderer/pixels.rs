8f154e94a76ab10bfb531bd6bf8dd79d70b4e8e8
seth <117851610+Sethispr@users.noreply.github.com>
2026-05-30 10:58:30 -0700

    refactor: replace w h to use Point and Size struct

--- src/renderer/pixels.rs
+++ src/renderer/pixels.rs
@@ -1,18 +1,43 @@
 use std::fmt::{Debug, Formatter, Result as FmtResult};
 
-/// raw uncompressed rgba image
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Point<T> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> Point<T> {
+    #[inline]
+    pub fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Size<T> {
+    pub width: T,
+    pub height: T,
+}
+
+impl<T> Size<T> {
+    #[inline]
+    pub fn new(width: T, height: T) -> Self {
+        Self { width, height }
+    }
+}
+
+/// pixels uncompressed rgba image
 /// this will act as a container that will replace our previous `image::RgbaImage`.
 pub struct RawCardImage {
-    pub width: u32,
-    pub height: u32,
+    pub size: Size<u32>,
     pub pixels: Box<[u8]>,
 }
 
 impl Debug for RawCardImage {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         f.debug_struct("RawCardImage")
-            .field("width", &self.width)
-            .field("height", &self.height)
+            .field("width", &self.size.width)
+            .field("height", &self.size.height)
             .field("pixels_len", &self.pixels.len())
             .finish()
     }
