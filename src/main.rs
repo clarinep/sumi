3ca2a3667d5998245f46a358c23af88c5d34ac96
Clarinep <konovbaraputra@gmail.com>
2026-06-13 14:36:21 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod logger;
 mod renderer;
 mod routes;
 
-use std::{env, error::Error, net::SocketAddr, sync::Arc};
+use std::{error::Error, net::SocketAddr, panic, sync::Arc};
 
 use axum::{routing::get, serve, Router};
 use mimalloc::MiMalloc;
@@ -16,6 +16,31 @@ use crate::{
     routes::{handle_metrics, handle_render_drop},
 };
 
+// aegis sets up a panic hook so we can format sys errors cleanly
+// as unexpected panics will give long unformatted backtraces.
+fn aegis() {
+    panic::set_hook(Box::new(|info| {
+        let msg = if let Some(s) = info.payload().downcast_ref::<&str>() {
+            s
+        } else if let Some(s) = info.payload().downcast_ref::<String>() {
+            s.as_str()
+        } else {
+            "sumi just crashed ..?"
+        };
+
+        if let Some(loc) = info.location() {
+            tracing::error!(
+                "sumi got sleepy..\n      reason: {}\n      location: {}:{}",
+                msg,
+                loc.file(),
+                loc.line()
+            );
+        } else {
+            tracing::error!("sumi got sleepy..\n      reason: {}", msg);
+        }
+    }));
+}
+
 // we use microsoft mimalloc as it handles memory better
 // it will only help when tokio is running multi threads
 #[global_allocator]
@@ -23,29 +48,29 @@ static ALLOC: MiMalloc = MiMalloc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "info");
-    }
+    aegis();
+
+    let filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("sumi=debug,info"));
 
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(filter)
         .event_format(logger::LogFormatter)
         .init();
 
-    let sumi_asc = include_str!("ascii.txt");
-    println!("\n\x1b[38;5;62m{}\x1b[0m", sumi_asc);
+    let sumi_greeting = include_str!("ascii.txt");
+    println!("\n\x1b[38;2;241;138;131m{}\x1b[0m", sumi_greeting);
 
     let cfg = Config::load();
 
-    tracing::info!("!! starting sumi on port {}", cfg.port);
     tracing::info!("baking in lexend deca font..");
     init_font();
 
     let renderer = match CardRenderer::new(cfg.cards_dir.clone()) {
         Ok(r) => r,
         Err(e) => {
-            tracing::error!("failed to wake sumi up..: {}", e);
-            std::process::exit(1);
+            tracing::error!("failed to wake sumi up..\n      reason: {}", e);
+            return Ok(());
         }
     };
     let state = Arc::new(renderer);
@@ -62,17 +87,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], cfg.port));
     let listener = TcpListener::bind(addr).await?;
 
-    tracing::info!("sumi ready at http://{addr}");
+    serve(listener, app).with_graceful_shutdown(nap()).await?;
 
-    serve(listener, app).with_graceful_shutdown(shutdown()).await?;
-
-    tracing::info!("sumi is draining tasks, please wait..");
+    tracing::info!("sumi is going to sleep, finishing tasks..");
     state.wait_for_tasks_to_finish().await;
 
     Ok(())
 }
 
-async fn shutdown() {
-    signal::ctrl_c().await.expect("ctrl+c");
+async fn nap() {
+    if let Err(e) = signal::ctrl_c().await {
+        tracing::error!("sumi failed to sleep..?({})", e);
+    }
     tracing::info!("you gave sumi way too much caffeine..");
 }
