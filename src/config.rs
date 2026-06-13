5c1c5f47565ddc03aac0e042e7d1ff67fa1c28bb
Clarinep <konovbaraputra@gmail.com>
2026-06-13 14:35:07 -0700

    Update Config::load to use user home directory
    Refactor config loading to use user home directory for cards path.

--- src/config.rs
+++ src/config.rs
@@ -9,16 +9,23 @@ pub struct Config {
 impl Config {
     pub fn load() -> Self {
         // read from env so its easier to setup to docker etc.
-        // for now we dont have env set but we will use our default path and default port 8888
-        // quick note to velo, kartu ditemuin di folder huty bukan di folder sumi, folder asset sini
-        // cuman buat font nya saja buat nomor print di kartu drop.
-        // (You can pass CARDS_DIR=... before running or use the default assets/cards)
-        let cards_dir =
-            env::var("CARDS_DIR").map_or_else(|_| PathBuf::from("assets/cards"), PathBuf::from);
+        // you can pass CARDS_DIR env before running
+        // for now we will use our default path and default port 8888
+        let home = env::var_os("USERPROFILE").or_else(|| env::var_os("HOME"));
+        let default_cards_dir = home
+            .map(PathBuf::from)
+            .map(|p| p.join("Documents").join("huty").join("cards"))
+            .unwrap_or_else(|| PathBuf::from("assets/cards"));
+
+        let cards_dir = env::var("CARDS_DIR").map_or_else(|_| default_cards_dir, PathBuf::from);
 
         let port = env::var("PORT").ok().and_then(|s| s.parse().ok()).unwrap_or(8888);
 
-        tracing::info!("config loaded (port: {}, cards_dir: {})", port, cards_dir.display());
+        tracing::info!(
+            "config loaded\n      port: [{}]\n      card: [{}]",
+            port,
+            cards_dir.display()
+        );
 
         Self { port, cards_dir }
     }
