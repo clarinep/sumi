f8ae996bf99d85fef0c5a9f4b5be056a44b7c2fe
Clarinep <konovbaraputra@gmail.com>
2026-07-20 08:44:04 +0700

    Update metrics.rs

--- src/metrics.rs
+++ src/metrics.rs
@@ -9,12 +9,14 @@ pub struct Metrics {
 }
 
 impl Metrics {
+    #[inline]
     pub fn record_success(&self, bytes: u64, render_time_ms: u64) {
         self.successful_renders.fetch_add(1, Ordering::Relaxed);
         self.total_image_bytes.fetch_add(bytes, Ordering::Relaxed);
         self.total_render_time_ms.fetch_add(render_time_ms, Ordering::Relaxed);
     }
 
+    #[inline]
     pub fn record_failure(&self) {
         self.failed_renders.fetch_add(1, Ordering::Relaxed);
     }
