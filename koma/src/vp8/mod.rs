3d63288b123122ff68150a7bb76904758f614b50
Clarinep <konovbaraputra@gmail.com>
2026-08-18 15:33:46 +0700

    Update mod.rs

--- koma/src/vp8/mod.rs
+++ koma/src/vp8/mod.rs
@@ -6,6 +6,7 @@
 pub mod bool_coder;
 pub mod config;
 pub mod frame;
+pub mod header_tables;
 pub mod tables;
 pub mod transform;
 
@@ -17,8 +18,8 @@ pub use config::{EncoderConfig, EncoderConfigBuilder};
 pub use frame::{encode_coeffs_block, encode_lossy_frame, quality_to_q_index};
 #[doc(inline)]
 pub use tables::{
-    AC_QLOOKUP, DC_QLOOKUP, PCAT1, PCAT2, PCAT3, PCAT4, PCAT5, PCAT6, PROB_EOB, PROB_ONE, PROB_TWO,
-    PROB_ZERO, VP8_START_CODE, ZIGZAG,
+    AC_QLOOKUP, DC_QLOOKUP, PCAT1, PCAT2, PCAT3, PCAT4, PCAT5, PCAT6, PROB_EOB, PROB_ONE,
+    PROB_TWO, PROB_ZERO, VP8_START_CODE, ZIGZAG,
 };
 #[doc(inline)]
 pub use transform::{fdct_4x4, forward_wht_4x4, idct_4x4, inverse_wht_4x4};
